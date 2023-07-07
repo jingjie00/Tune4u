a027e2c16592c48b692fa7166296d90f9cd5aad5
kmy-01 <kamminyi@gmail.com>
2023-07-08 04:55:21 +0800

    Added some magic

--- public/assets/js/easterEgg.js
+++ public/assets/js/easterEgg.js
@@ -1,21 +1,25 @@
 const createEasterEgg = () => {
-    const easterEgg = document.createElement('img')
-    const easterEggContainer = document.getElementById('easterEgg')//places to appear egg
+  const easterEgg = document.createElement("img");
+  const easterEggContainer = document.getElementById("easterEgg"); //places to appear egg
 
-    easterEgg.src = './assets/img/easterEgg.png'
-    easterEgg.classList.add('easter-egg', 'animate-in')
+  easterEgg.src = "./assets/img/easterEgg.png";
+  easterEgg.classList.add("easter-egg", "animate-in");
 
-    easterEgg.addEventListener('click', event => {
-        event.currentTarget.classList.add('break')
+  easterEgg.addEventListener("click", (event) => {
+    event.currentTarget.classList.add("break");
 
-        party.confetti(event.currentTarget, {
-            spread: 25,
-            size: 0.7,
-            count: 40
-        })
-    })
+    party.confetti(event.currentTarget, {
+      spread: 25,
+      size: 0.7,
+      count: 40,
+    });
+  });
 
-    easterEggContainer.append(easterEgg)
+  easterEggContainer.append(easterEgg);
 
-    setTimeout(() => easterEgg.classList.remove('animate-in'), 300)
-}
+  setTimeout(() => easterEgg.classList.remove("animate-in"), 300);
+};
+
+const testing = () => {
+  createEasterEgg();
+};
