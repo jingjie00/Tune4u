0408d9e1bb832a4a69eafff3b593085eb49609c0
TanMoon20 <tanseemoon@outlook.com>
2023-07-08 00:08:43 +0800

    connect database successfully

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -1,32 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyBct7eRux52dAVJ_8_YFNPz1zBm4sCzJB8",
-
     authDomain: "overflowingfeature.firebaseapp.com",
-  
     databaseURL: "https://overflowingfeature-default-rtdb.asia-southeast1.firebasedatabase.app",
-  
     projectId: "overflowingfeature",
-  
     storageBucket: "overflowingfeature.appspot.com",
-  
     messagingSenderId: "275715689132",
-  
     appId: "1:275715689132:web:86d867c52de9808aad2d09",
-  
     measurementId: "G-JW9RVHZRGS"
-};
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
-
-    
